b3f085083edf6b7d65d07bd3f7ee2488d928e158
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-03 23:01:12 +0200

    initial project

---
+++ Gallery/Gallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gallery
+//
+//  Created by Tanguy Hélesbeux on 03/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gallery/Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gallery
+//
+//  Created by Tanguy Hélesbeux on 03/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
