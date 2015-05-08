9ec671bab4ffd95a23bc03eafff83e3a18a2948f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-08 17:49:08 +0200

    added GASettingsDetailVC

---
+++ Gallery/Gallery/Controllers/DetailViewControllers/GASettingsDetailVC/GASettingsDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  GASettingsDetailVC.h
+//  Gallery
+//
+//  Created by Tanguy Hélesbeux on 08/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GASettingsDetailVC : UITableViewController
+
+@end
