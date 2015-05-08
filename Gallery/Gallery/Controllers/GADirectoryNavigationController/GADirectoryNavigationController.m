86a279588bf8b95f7b40ad7bf45a502f449de760
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-08 16:01:59 +0200

    added settings button

--- Gallery/Gallery/Controllers/GADirectoryNavigationController/GADirectoryNavigationController.m
+++ Gallery/Gallery/Controllers/GADirectoryNavigationController/GADirectoryNavigationController.m
@@ -10,6 +10,8 @@
 
 @interface GADirectoryNavigationController ()
 
+@property (strong, nonatomic) UIBarButtonItem *settingsButton;
+
 @property (strong, nonatomic) GADirectory *rootDirectory;
 
 @end
@@ -28,14 +30,42 @@
     if (self) {
         self.rootDirectory = rootDirectory;
         self.navigationBar.translucent = NO;
+        
+        [self initializeToolbar];
     }
     return self;
+}
+
+#pragma mark - Configuration
+
+- (void)initializeToolbar {
+    self.toolbarHidden = NO;
+    
+//    Buttons are linked to rootViewController
+    [[self.viewControllers firstObject] setToolbarItems:@[self.settingsButton]];
 }
 
 #pragma mark - Getters & Setters
 
 - (GADirectoryInspectorVC *)rootViewController {
     return (GADirectoryInspectorVC *)[self.viewControllers firstObject];
+}
+
+- (UIBarButtonItem *)settingsButton {
+    if (!_settingsButton) {
+        NSString *title = NSLocalizedString(@"SETTINGS", nil);
+        _settingsButton = [[UIBarButtonItem alloc] initWithTitle:title
+                                                           style:UIBarButtonItemStylePlain
+                                                          target:self
+                                                          action:@selector(settingsButtonHandler)];
+    }
+    return _settingsButton;
+}
+
+#pragma mark - Handlers
+
+- (void)settingsButtonHandler {
+    [self pushViewController:[UIViewController new] animated:YES];
 }
 
 @end
