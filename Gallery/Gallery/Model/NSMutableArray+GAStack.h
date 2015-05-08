d0ec58aeba79829d1753c56bbb35362c2215f3a8
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-08 11:56:50 +0200

    added Stack functions to NSMutableArray

---
+++ Gallery/Gallery/Model/NSMutableArray+GAStack.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+GAStack.h
+//  Gallery
+//
+//  Created by Tanguy Hélesbeux on 08/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (GAStack)
+
+@end
---
+++ Gallery/Gallery/Model/NSMutableArray+GAStack.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+GAStack.m
+//  Gallery
+//
+//  Created by Tanguy Hélesbeux on 08/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import "NSMutableArray+GAStack.h"
+
+@implementation NSMutableArray (GAStack)
+
+@end
