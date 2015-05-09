73031d49ca2b965616bfe0d40240422d7135f4aa
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-09 04:48:16 +0200

    clean

--- Gallery/Gallery/Controllers/DetailViewControllers/GAFileDetailNavigationController/GAFileInspectorVC/GAImageInspectorVC/GAImageInspectorVC.m
+++ Gallery/Gallery/Controllers/DetailViewControllers/GAFileDetailNavigationController/GAFileInspectorVC/GAImageInspectorVC/GAImageInspectorVC.m
@@ -32,35 +32,7 @@
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    if (YES) {
-        [self updateFramesWithImage:self.imageView.image];
-    } else {
-        self.isScrollViewInitialized = YES;
-        [self initializedFramesWith:self.imageView.image];
-    }
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
-{
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-    
-    // Code here will execute before the rotation begins.
-    // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
-    
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        
-        // Place code here to perform animations during the rotation.
-        // You can pass nil or leave this block empty if not necessary.
-        
-    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        
-        // Code here will execute after the rotation has finished.
-        // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
-//        _imageView = nil;
-//        self.imageView.image = [UIImage imageWithContentsOfFile:self.file.path];
-//        [self initializedFramesWith:self.imageView.image];
-        
-    }];
+    [self updateFramesWithImage:self.imageView.image];
 }
 
 #pragma mark - Getters & Setters
@@ -77,9 +49,7 @@
     if (!_imageView && _scrollView) {
         _imageView = [[UIImageView alloc] init];
         _imageView.contentMode = UIViewContentModeScaleAspectFit;
-        _imageView.backgroundColor = [UIColor blueColor];
         [_scrollView addSubview:_imageView];
-        _scrollView.backgroundColor = [UIColor redColor];
         _scrollView.delegate = self;
     }
     return _imageView;
@@ -91,41 +61,12 @@
     self.imageView.image = [UIImage imageWithContentsOfFile:self.file.path];
 }
 
-- (void)initializedFramesWith:(UIImage *)image {
-    if (image) {
-        NSLog(@"-------------");
-        NSLog(@"%.2f - %.2f", self.scrollView.frame.size.width, self.scrollView.frame.size.height);
-        NSLog(@"%.2f - %.2f", image.size.width, image.size.height);
-        NSLog(@"> %.2f - %.2f", self.imageView.frame.size.width, self.imageView.frame.size.height);
-        
-        self.imageView.frame = CGRectZero;
-        self.scrollView.contentSize = CGSizeZero;
-        CGFloat widthRatio = self.scrollView.frame.size.width/image.size.width;
-        CGFloat heightRatio = self.scrollView.frame.size.height/image.size.height;
-        CGFloat ratio = MIN(widthRatio, heightRatio);
-        
-        if (widthRatio < heightRatio) {
-            self.imageView.frame = CGRectMake(0, 0, image.size.width, self.scrollView.frame.size.height/ratio);
-        } else {
-            self.imageView.frame = CGRectMake(0, 0, self.scrollView.frame.size.width/ratio, image.size.height);
-        }
-        
-        self.scrollView.contentSize = self.imageView.image.size;
-        [self.scrollView setZoomScale:ratio animated:YES];
-        NSLog(@"> %.2f - %.2f", self.imageView.frame.size.width, self.imageView.frame.size.height);
-    }
-}
-
 - (void)updateFramesWithImage:(UIImage *)image {
     if (image) {
         [self.scrollView setZoomScale:1 animated:NO];
-        NSLog(@"-------------");
-        NSLog(@"%.2f - %.2f", self.scrollView.frame.size.width, self.scrollView.frame.size.height);
-        NSLog(@"%.2f - %.2f", image.size.width, image.size.height);
-        NSLog(@"> %.2f - %.2f", self.imageView.frame.size.width, self.imageView.frame.size.height);
-        
         self.imageView.frame = CGRectZero;
         self.scrollView.contentSize = CGSizeZero;
+        
         CGFloat widthRatio = self.scrollView.frame.size.width/image.size.width;
         CGFloat heightRatio = self.scrollView.frame.size.height/image.size.height;
         CGFloat ratio = MIN(widthRatio, heightRatio);
@@ -137,19 +78,11 @@
             frame = CGRectMake(0, 0, self.scrollView.frame.size.width/ratio, image.size.height);
         }
         
-        NSLog(@"> %.2f - %.2f", frame.size.width, frame.size.height);
         self.imageView.frame = frame;
-        NSLog(@"> %.2f - %.2f", self.imageView.frame.size.width, self.imageView.frame.size.height);
         self.scrollView.contentSize = self.imageView.image.size;
         [self.scrollView setZoomScale:ratio animated:NO];
-        NSLog(@"> %.2f - %.2f", self.imageView.frame.size.width, self.imageView.frame.size.height);
+//        [self.scrollView setMinimumZoomScale:ratio];
     }
-}
-
-- (CGFloat)minimumZoomForImage:(UIImage *)image inFrame:(CGRect)frame {
-    CGFloat widthRatio = frame.size.width/image.size.width;
-    CGFloat heightRatio = frame.size.height/image.size.height;
-    return MAX(widthRatio, heightRatio);
 }
 
 #pragma mark - UIScrollViewDelegate
