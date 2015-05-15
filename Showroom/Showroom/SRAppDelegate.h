4a67ccc009d288f299bed0d80e56c65dba91b2a2
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-15 20:43:53 +0200

    new project

---
+++ Showroom/Showroom/SRAppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Showroom
+//
+//  Created by Tanguy Hélesbeux on 15/05/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface SRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
