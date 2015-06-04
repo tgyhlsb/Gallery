86808ee26340e25e3e01ced6a8968353b109e706
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-06-04 22:34:25 +0200

    deleted old vc

---
+++ Showroom/Showroom/Controllers/SRDiaporamaViewController/SRDiaporamaViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRDiaporamaViewController.h
+//  Showroom
+//
+//  Created by Tanguy Hélesbeux on 04/06/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRDiaporamaViewController : UIViewController
+
+@end
---
+++ Showroom/Showroom/Controllers/SRDiaporamaViewController/SRDiaporamaViewController.m
@@ -0,0 +1,37 @@
+//
+//  SRDiaporamaViewController.m
+//  Showroom
+//
+//  Created by Tanguy Hélesbeux on 04/06/2015.
+//  Copyright (c) 2015 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import "SRDiaporamaViewController.h"
+
+@interface SRDiaporamaViewController ()
+
+@end
+
+@implementation SRDiaporamaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
