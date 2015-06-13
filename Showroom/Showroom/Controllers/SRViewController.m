ba111790ff6c45c564237d52d2c7dc7edba9c46f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-06-13 20:05:35 +0200

    updated banner size and back button titles

--- Showroom/Showroom/Controllers/SRViewController.m
+++ Showroom/Showroom/Controllers/SRViewController.m
@@ -12,11 +12,12 @@
 
 - (void)setTitleWithAppIcon {
     
-    UIImage *image = [UIImage imageNamed:@"banner-small-icon.png"];
+    UIImage *image = [UIImage imageNamed:@"banner.png"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     imageView.frame = CGRectMake(0, 0, 300, 44);
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     self.navigationItem.titleView = imageView;
+    self.title = @"";
 }
 
 @end
