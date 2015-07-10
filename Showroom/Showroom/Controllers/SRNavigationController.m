4ff4b237b9fafae5cd475b5f8a0cd7b62cdc78c0
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-07-10 10:06:15 +0200

    set title white in navigation bars

--- Showroom/Showroom/Controllers/SRNavigationController.m
+++ Showroom/Showroom/Controllers/SRNavigationController.m
@@ -16,6 +16,8 @@
     self.navigationBar.translucent = NO;
     self.navigationBar.barTintColor = [UIColor colorWithRed:0.12f green:0.45f blue:0.66f alpha:1.00f];
     self.navigationBar.tintColor = [UIColor whiteColor];
+    
+    self.navigationBar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, nil];
 }
 
 @end
